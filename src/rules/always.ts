fd7dd53c9c5bae9d0518084c611eda517c19b591
Jens Simon <jens.simon80@googlemail.com>
2023-06-25 22:17:01 +0200

    feat(go asi!): go ASI!

--- src/rules/always.ts
+++ src/rules/always.ts
@@ -1,13 +1,13 @@
-import type { Rule } from 'eslint';
+import type { Rule } from 'eslint'
 
 const always: Rule.RuleModule = {
   create: (context) => ({
     Program(node) {
-      const { options } = context;
-      const [option] = options;
-      context.report({ node, message: option?.message ?? 'This rule reports always.' });
+      const { options } = context
+      const [option] = options
+      context.report({ node, message: option?.message ?? 'This rule reports always.' })
     },
   }),
-};
+}
 
-export default always;
+export default always
