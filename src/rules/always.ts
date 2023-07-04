cbedae34dedd386bac9781c5bde20299a226399f
Jens Simon <jens.simon80@googlemail.com>
2023-07-04 23:45:28 +0200

    style: minor style changes

--- src/rules/always.ts
+++ src/rules/always.ts
@@ -3,9 +3,12 @@ import type { Rule } from 'eslint'
 const always: Rule.RuleModule = {
   create: (context) => ({
     Program(node) {
-      const { options } = context
-      const [option] = options
-      context.report({ node, message: option?.message ?? 'This rule reports always.' })
+      const [option] = context.options
+
+      context.report({
+        node,
+        message: option?.message ?? 'This rule reports always.',
+      })
     },
   }),
 }
