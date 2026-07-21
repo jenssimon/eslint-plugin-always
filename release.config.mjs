e352532238b97b431c51e11c166752fea7e50520
Jens Simon <jens.simon@positiveminds.io>
2026-07-21 13:11:22 +0200

    ci: fix semantic release

---
+++ release.config.mjs
@@ -0,0 +1,14 @@
+export default {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: true,
+      },
+    ],
+    "@semantic-release/github",
+  ],
+}
